efa0fee02a50de325a31bb9fe987d501a5c38e03
jammisandy <109514629+jammisandy@users.noreply.github.com>
2024-01-01 07:19:33 -0600

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull sandeepjammi/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 sandeepjammi/simple-python-flask-app
+docker run -d -p 5000:5000 sandeepjammi/simple-python-flask-app 
